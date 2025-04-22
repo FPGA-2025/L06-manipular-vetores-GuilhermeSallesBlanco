25f96b71abe5c1e45536f2674ae53a2a17049c3b
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-04-21 22:07:59 -0300

    Adicionando tarefa realizada

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,6 +2,7 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-    // insira seu código aqui
+assign saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
+// Trocando a posição dos bytes dentro do vetor de saída
 
 endmodule
